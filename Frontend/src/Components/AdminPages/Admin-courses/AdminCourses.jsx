5bb8606b5fe0e710099fddaef090de9abe093281
Anurag0577 <anurag0577@gmail.com>
2025-05-08 23:31:31 +0530

    Work on Admin's Courses page

---
+++ Frontend/src/Components/AdminPages/Admin-courses/AdminCourses.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useState, useEffect} from 'react'
+
+function AdminCourses(){
+
+    return (
+        <div className='admin-course-page'>
+            <div className='admin-course-page-container'>
+                <div className='admin-course-heading'>My Courses</div>
+                <div className='admin-course-card-container'>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
